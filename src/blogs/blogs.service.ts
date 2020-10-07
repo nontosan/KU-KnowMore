4be13b2546df96bf0fcba6486280bff718625619
frostdragonite <fdragonite@gmail.com>
2020-10-07 18:22:14 +0700

    Blog Filter v1

--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -19,4 +19,30 @@ export class Blog_Service {
         return this.Blog_Repository.find({where: { _id: blog_id }});
     }
 
+    async findAllBlogsFilter(Obj): Promise<Blogs[]> {
+        var filter1 = Obj.type;
+        var filter2 = Obj.code;
+        var filter3 = Obj.name;
+
+        // ปล. ถ้ามีวิธีที่ดีกว่านี้ ก็แก้ด้วย   PS..IF THERE IS A BETTER WAY, PLEASE OPTIMIZE
+
+        if (filter1 && filter2 && filter3)
+            return this.Blog_Repository.find({where: {type:filter1, course_code:filter2, blog_name:filter3} });
+        else if (filter1 && filter2)
+            return this.Blog_Repository.find({where: {type:filter1, course_code:filter2} });
+        else if (filter1 && filter3)
+            return this.Blog_Repository.find({where: {type:filter1, blog_name:filter3} });
+        else if (filter2 && filter3)
+            return this.Blog_Repository.find({where: {course_code:filter2, blog_name:filter3} });
+        else if (filter1)
+            return this.Blog_Repository.find({where: {type:filter1} });
+        else if (filter2)
+            return this.Blog_Repository.find({where: {course_code:filter2} });
+        else if (filter3)
+            return this.Blog_Repository.find({where: {blog_name:filter3} });
+        else
+            return this.Blog_Repository.find();
+    }
+
+    
 }
