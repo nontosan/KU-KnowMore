51d2cf6b094cf80f78308693aa765d8c11d39c71
ohmigiko <71168325+ohmigiko@users.noreply.github.com>
2020-10-12 03:33:47 +0700

    heetad

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,62 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Input_Nameblog from './component/input_nameblog';
-import Input_Idclass from './component/input_idclass';
-import Input_Nameclass from './component/input_nameclass';
-import Input_Nameteacher from './component/input_nameteacher';
-import Confirm from './component/confirm';
+import './component/input.css'
+import Input_Blogname from './component/Input_Blogname';
+import Input_Subject from './component/Input_Subject';
+import Input_Coursecode from './component/Input_Coursecode';
+import Input_Teachername from './component/Input_Teachername';
+import * as yup from "yup";
+import Submit_Cancel from './component/Submit_Cancel';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 
 
 const App=()=> {
-  const [Nameblog, setNameblog]=useState("");
-  const [Nameteacher, setNameteacher]=useState("");
-  const [IDclass, setIDclass]=useState("");
-  const [Nameclass, setNameclass]=useState("");
-  const call=()=>{
-    alert(Nameblog)
-    alert(IDclass)
-    alert(Nameclass)
-    alert(Nameteacher)
-  }
-  
-  return (
-    <div className="bg_color">
-     <div className="Blog_Info">
-        <button onClick={call}>
-         check
-        </button>
-      <Input_Nameblog setNameblog={setNameblog} />
-      <Input_Idclass setIDclass={setIDclass} />
-      <Input_Nameclass setNameclass={setNameclass} />
-      <Input_Nameteacher setNameteacher={setNameteacher} />
-      <Confirm />
-    </div>
-    </div>
 
-  );
+  const [input,setInput] = useState({
+    blogname:'',
+    coursecode:'',
+    subject:'',
+    teacher:''
+  })  
+
+  const handleChange = (e:any) => {
+    const { target } = e
+    const { name } = target
+    const value = name ==='term' ? target.checked : target.value
+    
+    setInput({
+      ...input,
+      [name]: value
+    })
+  }
+
+  
+
+  const informationSchema = yup.object().shape({
+    blogname: yup.string().required('This field is required.'),
+    coursecode: yup.string().required('This field is required.'),
+    subject: yup.string().required('This field is required.'),
+    teacher: yup.string().required('This field is required.'),
+  });
+  
+  
+
+  return(
+    <div className = "bg_color">
+      <div className="Blog_Info">
+        <Input_Blogname change={handleChange}/>
+        <Input_Coursecode change={handleChange}/>
+        <Input_Subject coursecode={input.coursecode}/>
+        <Input_Teachername change={handleChange}/>
+        <Submit_Cancel input={input}/>
+      </div>  
+    </div>
+  )
 }
 
 export default App;
