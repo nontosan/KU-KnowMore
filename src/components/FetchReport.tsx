1fc15406d16b0e75f99d97df2548b459205f4a49
pattarathorn <71335122+pattarathorn@users.noreply.github.com>
2020-10-19 20:53:39 +0700

    check id = id

--- src/components/FetchReport.tsx
+++ src/components/FetchReport.tsx
@@ -1,6 +1,7 @@
 import React,{useState,useEffect}from 'react';
 import { ListGroup, NavLink } from 'react-bootstrap';
 import { Reports_data } from "../interfaces/reports";
+import { Blog } from "../interfaces/blog";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
     BrowserRouter as Router,
@@ -15,48 +16,66 @@ const FetchReport = () => {
 
     const [hasError,setErrors] = useState<boolean>(false)
     const [reportedBlog,setReport] = useState<Reports_data[]>([])
+    const [DataBlog,setBlog] = useState<Blog[]>([])
     
-    async function fetchData(){
+    async function fetchDataRe(){
         const res =  await fetch("http://188.166.178.33:3000/reports/")
         res
             .json()
             .then(res => setReport(res))
             .catch(err => setErrors(err))
     }
+    async function fetchDataBlog(){
+        const res =  await fetch("http://188.166.178.33:3000/blogs/")
+        res
+            .json()
+            .then(resp => setBlog(resp))
+            .catch(errb => setErrors(errb))
+    }
 
     useEffect(() =>{ 
-        fetchData();
+        fetchDataRe();
+        fetchDataBlog();
     },[])
-
-    const ReadUserBlog = (a: any) => {
-        const params = useParams()
-        console.log('param = ',params)
-        return {a}
-    }
-    
 
     const DeleteBlog = () => {
         console.log('This blog was deleted')
     }
 
+
+    const listreport = reportedBlog.map(rblog => (
+        <ListGroup.Item className="blogcontainer" >
+            <div className="element">                        
+                    {rblog.id}
+            </div>
+        </ListGroup.Item>
+    ))
+
+    const listdata = DataBlog.map(blog => (
+        <ListGroup.Item className="blogcontainer" >
+            <div className="element">                        
+                    {blog.id}
+            </div>
+        </ListGroup.Item>
+    ))
+    const Fuck = () => {
+        if (listdata === listreport)
+        {
+            console.log('id = id')
+        }
+        console.log('Fuck')
+    }
+    
+    
     return(
         <div>
-            <h2>Reported Blog</h2>
-                {reportedBlog.map(blog => (
-                    <ListGroup.Item className="blogcontainer" >
-                        <div className="element">
-                            <Link to="/${blog.id }">
-                                {blog.id}
-                                {/**{ReadUserBlog(blog.id)}*/}
-                            </Link>
-                            <button onClick = {DeleteBlog}>
-                                Delete
-                            </button>
-                        </div>
-                    </ListGroup.Item>
-                ))}
+            <button onClick = {Fuck}> Check </button>
+            {listreport}
+            <h1>Data</h1>
+            {listdata}
+            
         </div>
     )
 };
 
-export default FetchReport   
+export default FetchReport 
