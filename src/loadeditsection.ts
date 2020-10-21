fbfb7aa3be4ea038acf679488d811d9ea4c62aa6
pattarathorn <71335122+pattarathorn@users.noreply.github.com>
2020-10-21 17:03:06 +0700

    Delete loadeditsection.ts

--- src/loadeditsection.ts
+++
@@ -1,8 +0,0 @@
-import { Section_Edit } from "../interfaces/section_edit"
-
-export async function fetchsection():Promise<Section_Edit[]>{
-    const res = await fetch("http://188.166.178.33:3000/blogs/")
-    const section = await res.json()
-    return section
-} 
-export default {fetchsection}
