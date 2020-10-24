fb6f84f46f167d8cc0198ffd1563659c9f59dc2c
frostdragonite <fdragonite@gmail.com>
2020-10-24 12:04:08 +0700

    Search Blogs (courses updated)

--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -111,7 +111,7 @@ export class Blog_Service {
             dateList.sort(function(a,b){
                 return b[0] - a[0];
               });
-            console.log(dateList)
+            // console.log(dateList)
             // Sort JSON
             var tmp = []
             for (var i = 0; i < dateList.length; i++) {
@@ -120,7 +120,7 @@ export class Blog_Service {
             }
             res = tmp;
         }
-        console.log(res);
+        // console.log(res);
         console.log("Order Type: " + order_type)
         return res;
     }
@@ -139,73 +139,102 @@ export class Blog_Service {
     }
 
     async findAllBlogsSearch(Obj): Promise<Blogs[]> {
-        var tmp = [];
         var res = [];
         var course_list = [];
+        var course_list_blogs = [];
+        var course_list_pos = [];
         var type = (Obj.type ? (Obj.type == 1 ? "knowledge":(Obj.type == 2 ?"review":undefined)) : undefined);
         var sname = Obj.sname;
         var pname = Obj.pname;
         var order = (Obj.order ? parseInt(Obj.order) : 1);
+        
+        if (sname || pname) {
+            // Find all course_id,
+            var tmp = await this.courseService.findAllCourses()
 
-        // Check if sname == code , else if sname == subjectname
-        if (sname && !isNaN(sname)) {
-            // Find all course_id with the same course_code , then save into course_list
-            tmp = await this.courseService.findAllCoursesCode(sname)
-            for (var x in tmp) {
-                course_list.push(tmp[x].id.toString());
+            // for sname
+            if (sname && isNaN(sname)) {
+                // console.log("Start Search SName")
+                // if (pname) console.log("And PName")
+
+                // Search the thing
+                for (var x in tmp) {
+                    // Check undefined (There is undefined in the database for some reason)
+                    if (tmp[x].NameEn != undefined && tmp[x].NameTh != undefined) {
+                        //  then save (if sname is substring to course_name {case insensitive}) into course_list
+                        if (tmp[x].NameEn.toLowerCase().includes(sname.toLowerCase()) || tmp[x].NameTh.includes(sname)) {
+                            // as well for pname
+                            if (pname && tmp[x].Teacher != undefined) {
+                                if (tmp[x].Teacher.includes(pname)) course_list.push(tmp[x].id.toString());
+                            } else course_list.push(tmp[x].id.toString());
+                        }       
+                    }
+                }
             }
-        } else if (sname && isNaN(sname)) {
-            // Find all course_id, then save (if sname is substring to course_name {case insensitive}) into course_list
-            tmp = await this.courseService.findAllCourses()
-            for (var x in tmp) {
-                if (tmp[x].course_name.toLowerCase().includes(sname.toLowerCase()))
-                    course_list.push(tmp[x].id.toString());
+            // for pname only
+            else if (pname) {
+                console.log("Start Search PName")
+                for (var x in tmp) {
+                    if (tmp[x].Teacher != undefined) {
+                        if (tmp[x].Teacher.includes(pname))
+                            course_list.push(tmp[x].id.toString());
+                    }
+                }
             }
         }
 
-        // If include teacher name
-        if (pname) {
-            var course_list_tmp = [];
-            tmp = await this.courseService.findAllCoursesTeacher(pname);
-            // 2 Case,  both sname && pname   or    only pname
-            if (sname) {
-                // Result course_list are the intersection of this and sname_course_list
-                for (var x in tmp) {
-                    if (course_list.includes(tmp[x].id.toString()))
-                        course_list_tmp.push(tmp[x].id.toString());
-                }
-            } else {
-                // Just save it
-                for (var x in tmp) {
-                    course_list_tmp.push(tmp[x].id.toString());
-                }
-            }
-            course_list = course_list_tmp;
-        }
-        
-        // console.log("Course_List")
-        // console.log(course_list)
-        
+        // console.log("Start Search PName")
+        // // If include teacher name
+        // if (pname) {
+        //     var course_list_tmp = [];
+        //     var tmp = await this.courseService.findAllCoursesTeacher(pname);
+        //     // 2 Case,  both sname && pname   or    only pname
+        //     if (sname) {
+        //         // Result course_list are the intersection of this and sname_course_list
+        //         for (var x in tmp) {
+        //             if (course_list.includes(tmp[x].id.toString()))
+        //                 course_list_tmp.push(tmp[x].id.toString());
+        //         }
+        //     } else {
+        //         // Just save it
+        //         for (var x in tmp) {
+        //             course_list_tmp.push(tmp[x].id.toString());
+        //         }
+        //     }
+        //     course_list = course_list_tmp;
+        // }
+
+
         // Make JSON
         // If searched for sname or pname: Check inside , else normal search
         if (sname || pname) {
+            // console.log("Preload")
+            // Preload
+            var tmp2 = await this.Blog_Repository.find();
+            for (var x in tmp2) {
+                course_list_blogs.push(tmp2[x].course_id.toString());
+            }
+            for (var x in course_list_blogs) {
+                if (course_list.includes(course_list_blogs[x])) course_list_pos.push(course_list_blogs[x]);
+            }
+            // console.log("Start Making JSON")
             // Specific Type, else all types
-            for (var i = 0; i < course_list.length; i++) {
+            for (var i = 0; i < course_list_pos.length; i++) {
                 // Save all the courses with specific type into res , else any types
-                if (type) var tmp2 = await this.Blog_Repository.find({where: {type:type, course_id: course_list[i]} });
-                else var tmp2 = await this.Blog_Repository.find({where: {course_id: course_list[i]} });
+                if (type) tmp2 = await this.Blog_Repository.find({where: {type:type, course_id: course_list_pos[i]} });
+                else tmp2 = await this.Blog_Repository.find({where: {course_id: course_list_pos[i]} });
                 //Add into Result
                 if (tmp2.length != 0) for (var j = 0; j < tmp2.length; j++) res.push(tmp2[j]);
             }
         } else {
+            // console.log("Start Making JSON")
             // Specific Type or All Types
             if (type) res.push( await this.Blog_Repository.find({where: {type:type} }) );
             else res.push( await this.Blog_Repository.find());
             res = res[0]
         }
         
-        console.log(res)
-
+        // console.log("Start Sorting")
         return this.sortJSON(res,order);
     }
 
