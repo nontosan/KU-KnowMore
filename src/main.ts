7b809382afbd8930b7be9614055ba2b5ca663b9c
nontosan <non.vit@ku.th>
2020-11-22 23:15:10 +0700

    file size limit change

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(3000);
